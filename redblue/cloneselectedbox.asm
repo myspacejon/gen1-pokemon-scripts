b02b3da52ca36291f633b121ebff3bcef8548d5e
myspace jon <217248955+myspacejon@users.noreply.github.com>
2025-06-20 15:28:31 -0700

    We intentionally omit checksum calculations and the write to set banking mode, as they are not validated or required by the game.

--- redblue/cloneselectedbox.asm
+++ redblue/cloneselectedbox.asm
@@ -1,24 +1,24 @@
+; ===================================================================================
 ; Routine: CloneWRAMBoxtoSRAMBox2
 ;
 ; Purpose: Clones Pokémon in the currently selected box to Box 2. Box 2 is
 ;          overwritten in this process.
 ; 
-; Method:  Clones the box currently loaded in WRAM ($DA80) into Box 2 of
-;          the SRAM ($A462) and then updates all necessary checksums to
-;          ensure the save file remains valid.
+; Notes:   We intentionally omit checksum calculations and the write to
+;          set banking mode, as they are not validated or required by the
+;          game. See commit e4b5eaac0f03b43a513d3cdfd20f85d46ce97cb0 for reference.
+;
 ; ===================================================================================
 
 CloneWRAMBoxtoSRAMBox2::
-	; Enables write-access to the cartridge's save memory (SRAM) and
-	; configures the memory bank controller (MBC1) to make SRAM Bank 2
-	; (containing Boxes 1-6) the active bank.
+
+	; Enables write-access to the cartridge's save memory (SRAM) and selects
+	; SRAM Bank 2 (containing Boxes 1-6) as the active bank.
 	ld   a, $0A
 	ld   [$0000], a
-	ld   a, $1
-	ld   [$6000], a
 	ld   a, $2
 	ld   [$4000], a
-
+	
 	; Sets up the arguments for the CopyData routine and calls it. This
 	; copies the entire 1122-byte box data from the WRAM buffer to the
 	; destination (Box 2) in SRAM.
@@ -27,35 +27,8 @@ CloneWRAMBoxtoSRAMBox2::
 	ld   bc, $0462
 	call $00B5
 
-	; Recalculates the main checksum for the entire SRAM bank. This is done
-	; with a manual bankswitch because the SAVCheckSum routine requires its
-	; arguments in HL and BC, creating a conflict with the Bankswitch
-	; routine. The resulting checksum is safely preserved.
-	ld   hl, $A000
-	ld   bc, $1A4C
-	ld   a, [$FF50]
-	push af
-	ld   a, $1C
-	ld   [$2000], a
-	call $7856
-	push af
-	pop  hl
-	pop  af
-	ld   [$2000], a
-	ld   a, l
-	ld   [$BA4C], a
-
-	; Recalculates the individual checksum for all 6 boxes in the bank.
-	; This uses the game's built-in Bankswitch routine ($35D6),
-	; which is safe and efficient here as the target routine
-	; CalcIndividualBoxCheckSums takes no arguments.
-	ld   b, $1C
-	ld   hl, $7863
-	call $35D6
-
-	; Disables write-access to SRAM and reverts the MBC1 controller back
-	; to its default ROM banking mode.
+	; Disables write-access to SRAM to protect the save data before
+	; returning from the subroutine.
 	xor  a
-	ld   [$6000], a
 	ld   [$0000], a
 	ret
